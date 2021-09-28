06d9afe49294b9c4a7dc115b284615738f423470
Mohammed Pasha <mk.pasha@hotmail.com>
2021-09-28 00:31:00 -0400

    refactor: enable uuid for generating rooms

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -1,10 +1,9 @@
 import React from 'react';
-//import { v1 as uuid } from 'uuid';
+import { v1 as uuid } from 'uuid';
 
 const CreateRoom = props => {
   function create() {
-    //const id = uuid();
-    const id = Math.floor(Math.random() * "0xF");
+    const id = uuid();
     props.history.push(`/room/${id}`);
     console.log(`${id} room created`); //DELETE
   }
